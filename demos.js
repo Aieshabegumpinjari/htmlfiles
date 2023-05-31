495bd37c31d036174f8832762bdb7e0a5ce50f95
Aieshabegumpinjari <135025389+Aieshabegumpinjari@users.noreply.github.com>
2023-05-31 09:52:44 +0530

    Add files via upload

---
+++ demos.js
@@ -0,0 +1,4 @@
+document.write(" i am aiesha"+"<br>")
+document.write("345678")
+console.log("10+20")
+console.log("i tooo")
---
+++ uuu.js
@@ -0,0 +1,49 @@
+/*var  name="rajkumar"+"<br>"
+var  age=25
+let place="aiesha"+"<br>"
+let add="kjh"+"<br>"
+ const mobile=123456789+"<br>"
+ document.write(mobile)
+document.write(name)
+console.log(name)
+name="rani"
+document.write(name)
+document.write(add)*/
+var name="aiesha"
+function js(){
+    var age=23
+    document.write(name+"<br>")
+    document.write(age+"<br>")
+}
+js()
+document.write(name)
+document.write(age)
+/*var std={
+
+    name:"aiesha",
+    rolno:467,
+    email:"adsse@4545gmail.com"
+}*/
+/*for(let x=0;x<5;x++)
+    
+{
+document.write("3*x=")
+document.write(3*x+"<br>")
+}*/
+/*let age=16
+if(age>=18){
+    document.write("your age is greater than 18")
+    document.write("your age="+age)
+}
+else if(age<18)
+{
+    document.write("your age is lesser than the 18"+"<br>")
+    document.write("your age is="+age)
+}
+else
+{
+    document.write("your age is equal to 18")
+}*/
+
+
+
